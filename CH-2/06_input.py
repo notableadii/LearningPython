d5839856a2681343dace01c5f19807428352c905
Aditya Shah <82594256+notableadii@users.noreply.github.com>
2025-05-10 09:14:48 +0530

    update

--- CH-2/06_input.py
+++ CH-2/06_input.py
@@ -20,3 +20,19 @@ print("Type of a after converting = ", type(c)) # <class 'str'> because a is a s
 print("Type of b after converting = ", type(d)) # <class 'str'> because b is a string
 
 print("Sum of a and b = ", c + d) # sum of a and b
+
+# an easier and direct way to convert a string to an integer is to use the eval function
+
+print("Sum of a and b after using evaluate funciton = ", eval(a) + eval(b)) # sum of a and b
+
+# eval function is used to evaluate a string as a python expression
+
+# another way to convert a string to an integer is directly take input from the user as an integer
+
+e = int(input("Enter a number: ")) # a is an integer
+f = int(input("Enter a number: ")) # b is an integer
+
+print("Sum of e and f = ", e + f) # sum of e and f
+
+print("Type of e = ", type(e)) # <class 'int'> because e is an integer
+print("Type of f = ", type(f)) # <class 'int'> because f is an integer
