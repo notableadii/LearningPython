e8a7706f89cd9c3cbfa36a231552cf1b8f70823e
Aditya Shah <82594256+notableadii@users.noreply.github.com>
2025-05-11 23:22:24 +0530

    update

--- CH-3/problem.py
+++ CH-3/problem.py
@@ -10,4 +10,4 @@ letter = "[NAME] You are selected for the [POSITION] position in [COMPANY]."
 
 print(letter.replace("[NAME]", name).replace("[POSITION]", "Software Engineer").replace("[COMPANY]", "Turing")) # this will replace the [NAME] with the name entered by the user and [POSITION] with Software Engineer and [COMPANY] with Turing
 
-# problem 3
+# strings are immutable in python means u cannot change the string but u can create a new string with the changes which is done in the above code print(letter.replace("[NAME]", name).replace("[POSITION]", "Software Engineer").replace("[COMPANY]", "Turing")) which is a new string and the original string is not changed
