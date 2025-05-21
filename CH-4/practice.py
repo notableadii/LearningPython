879f0340fd969b5a7a2b0f7c81163caddd6dc099
Aditya Shah <82594256+notableadii@users.noreply.github.com>
2025-05-21 12:43:19 +0530

    update

---
+++ CH-4/practice.py
@@ -0,0 +1,55 @@
+# problem 1
+
+# marks = []
+
+# f1 = input("Enter Marks Here: ")
+# marks.append(f1)
+# f2 = input("Enter Marks Here: ")
+# marks.append(f2)
+# f3 = input("Enter Marks Here: ")
+# marks.append(f3)
+# f4 = input("Enter Marks Here: ")
+# marks.append(f4)
+# f5 = input("Enter Marks Here: ")
+# marks.append(f5)
+
+# print(marks)
+
+#problem 2
+
+
+# marks = []
+# f1 = int(input("Enter Marks Here: "))
+# marks.append(f1)
+# f2 = int(input("Enter Marks Here: "))
+# marks.append(f2)
+# f3 = int(input("Enter Marks Here: "))
+# marks.append(f3)
+# f4 = int(input("Enter Marks Here: "))
+# marks.append(f4)
+# f5 = int(input("Enter Marks Here: "))
+# marks.append(f5)
+
+
+# marks.sort() # sort the list in place
+# print(marks)
+
+#problem 3
+
+# a = (1, 2, "Hello", False, 3.5, 1, 1) # tuple with multiple elements
+
+# a[2] = "Hi" # tuple is immutable
+
+
+# problem 4
+
+l = [1, 2, 3, 4, 5]
+
+print(sum(l)) # sum function is used to find the sum of all elements in the list
+
+
+# problem 5
+
+a = (1, 6, 2, 3, 4, 5, 0, 0, 0, )
+
+print(a.count(0)) # count function is used to count the number of occurrences of an element in the tuple
