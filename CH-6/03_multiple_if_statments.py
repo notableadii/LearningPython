3409d9ae23f12c7193df9d6c3959af11c8489d5a
Aditya Shah <82594256+notableadii@users.noreply.github.com>
2025-05-25 19:52:10 +0530

    update

--- CH-6/03_multiple_if_statments.py
+++ CH-6/03_multiple_if_statments.py
@@ -4,7 +4,10 @@ a = int(input("Enter a number: "))  # Prompt user for input
 #if statement no 1
 if(a%2 == 0):  # Check if the number is even
     print("The number is even.")  # If true, print this message    
-#if statement no 
+
+#end of if statement no 1
+
+#if statement no 2
 if (a >= 18):  # Check if the number is greater than 18
     print("You are eligible to vote.")  # If true, print this message
     print("You can vote in the upcoming elections.")  # Additional message if eligible
@@ -14,3 +17,5 @@ elif(a < 0):  # Check if the number is negative
 
 else:  # If the condition is false
     print("You are not eligible to vote.")  # Print this message
+
+#end of if statement no 2
