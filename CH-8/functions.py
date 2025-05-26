1edc0f0ca7485de259262b29702960ff818ec7ed
Aditya Shah <82594256+notableadii@users.noreply.github.com>
2025-05-26 21:46:26 +0530

    update

---
+++ CH-8/functions.py
@@ -0,0 +1,12 @@
+# what are functions in python??
+# Functions in Python are reusable blocks of code that perform a specific task.
+
+a = 12
+
+b = 45
+
+c = 56
+
+average = (a + b + c) / 3
+
+print("The average of", a, ",", b, "and", c, "is:", average)
