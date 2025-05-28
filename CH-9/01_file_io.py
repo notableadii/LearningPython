6182ab8c55fee3620759593000d294c9d8ab8409
Aditya Shah <82594256+notableadii@users.noreply.github.com>
2025-05-28 16:17:24 +0530

    Files I/O

--- CH-9/01_file_io.py
+++ CH-9/01_file_io.py
@@ -5,3 +5,13 @@ a = "a very long string with emails"
 emails = []
 
 '''
+
+f = open("CH-9/file.txt", "r")
+data = f.read()
+print(data)
+f.close()
+
+f = open("CH-9/file.txt", "w")
+f.write("This is a new line")
+f.close()
+
