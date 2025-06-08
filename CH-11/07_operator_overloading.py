675a2c6c480123b7c7387f8b6f0080b36b6f6911
Aditya Shah <82594256+notableadii@users.noreply.github.com>
2025-06-08 19:21:25 +0530

    .

--- CH-11/07_operator_overloading.py
+++ CH-11/07_operator_overloading.py
@@ -1,10 +1,11 @@
 class Number:
     def __init__(self, n):
-        self.n =n
-    
-    def __add__(self, num):
+        self.n = n
+
+    def __add__(self, num):  # This method is called when the + operator is used
         return self.n + num.n
-        
+
+
 n = Number(5)
 m = Number(10)
 
--- CH-13/03_join.py
+++ CH-13/03_join.py
@@ -2,3 +2,5 @@ a = ["Aditya", "Subham", "Sahil", "Ankit"]
 
 final = " and ".join(a)
 print(final)
+# This will print "Aditya and Subham and Sahil and Ankit"
+# This code takes a list of names and joins them into a single string with " and " as the separator.
